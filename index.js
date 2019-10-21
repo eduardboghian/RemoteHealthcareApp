135ce7187da5c530008f4ce5fb8a307e5de6938d
edi4404 <ediboghian@yahoo.com>
2019-10-21 14:03:54 +0300

    1st

---
+++ index.js
@@ -0,0 +1,18 @@
+const cookieParser = require("cookie-parser")
+const path = require('path')
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static('./build'))
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, 'server', 'build', 'index.html'));
+    })
+}
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, console.log(`connected to port ${PORT}...`))
