fefb38f62a9b44fdcf27adadc3428ae87deb32cc
edi4404 <ediboghian@yahoo.com>
2019-11-12 14:21:25 +0200

    now it works

--- routes/auth.js
+++ routes/auth.js
@@ -82,7 +82,7 @@ router.post('/login/doctor', async (req, res)=> {
 
     if(! doc.approved ) return res.status(400).send('Your account was not approved yet!')
 
-    const token = jwt.sign({_id: doc._id, name: doc.name, email: doc.email, contactList: user.contactList}, process.env.TOKEN_SECRET)
+    const token = jwt.sign({_id: doc._id, name: doc.name, email: doc.email}, process.env.TOKEN_SECRET)
     res.header('authToken', token).send('logged in as doctor')
 
 })
